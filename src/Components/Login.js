6cb048debd757ff28311f564bdcb1ff672b83b9c
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-11-06 16:45:48 +0530

    added functionality to toggle between sign in and sign up

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import Header from './Header'
+import { useState } from 'react';
 
 const Login = () => {
+
+  const [isSign, setIsSignIn] = useState(true);
+
+  const toogleSignInForm = () => {
+    setIsSignIn(!isSign);
+  }
+
   return (
     <div>
       <Header />
@@ -11,9 +19,12 @@ const Login = () => {
           src="https://assets.nflxext.com/ffe/siteui/vlv3/ab4b0b22-2ddf-4d48-ae88-c201ae0267e2/0efe6360-4f6d-4b10-beb6-81e0762cfe81/IN-en-20231030-popsignuptwoweeks-perspective_alpha_website_large.jpg"
         />
       </div>
-      <form className="absolute bg-black w-[30%] p-12 mt-[6rem] mb-[9rem] mx-auto right-0 left-0 text-white 
-      rounded-sm bg-opacity-80">
-        <h1 className="font-bold text-xl py-4 ">Sign In </h1>
+      <form
+        className="absolute bg-black w-[30%] p-12 mt-[6rem] mb-[9rem] mx-auto right-0 left-0 text-white 
+      rounded-sm bg-opacity-80"
+      >
+        {isSign && <h1 className="font-bold text-xl py-4 ">Sign In </h1>}
+        {!isSign && <h1 className="font-bold text-xl py-4 ">Sign Up </h1>}
         <input
           type="text"
           placeholder="Email or phone number"
@@ -24,9 +35,21 @@ const Login = () => {
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700 rounded-sm"
         />
-        <button className="bg-red-900 p-4 my-6 w-full rounded-sm">
-          Sign In
-        </button>
+        {isSign && (
+          <button className="bg-red-900 p-4 my-6 w-full rounded-sm">
+            Sign In
+          </button>
+        )}
+        {!isSign && (
+          <button className="bg-red-900 p-4 my-6 w-full rounded-sm">
+            Sign Up
+          </button>
+        )}
+        {isSign && (
+          <p className="text-sm cursor-pointer" onClick={() => toogleSignInForm()}>
+            New to Netflix? Sign Up Now!
+          </p>
+        )}
       </form>
     </div>
   );
