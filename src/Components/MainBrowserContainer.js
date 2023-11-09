d03badcb9448d0016aa9a30b64d9008d88919daf
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-11-10 01:07:22 +0530

    added fucntionality to show random now playing movies in the main container

--- src/Components/MainBrowserContainer.js
+++ src/Components/MainBrowserContainer.js
@@ -8,8 +8,11 @@ const MainBrowserContainer = () => {
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
   
   if(!movies) return;
-
-  const { original_title, overview, id } = movies[0];
+  let min = 0;
+  let max = 20;
+  const movieNum = parseInt(Math.floor(Math.random() * (max - min + 1)) + min);
+  console.log(movieNum);
+  const { original_title, overview, id } = movies[movieNum];
   const mainVideo = movies[0];
   return (
     <div>
