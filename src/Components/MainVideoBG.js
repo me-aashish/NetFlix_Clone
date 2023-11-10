5b68904661e7bdf90e3d6356a0f63edd31bf1c2d
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-11-10 20:40:58 +0530

    added main BG video to run on loop

--- src/Components/MainVideoBG.js
+++ src/Components/MainVideoBG.js
@@ -11,8 +11,13 @@ const MainVideoBG = ({ movieId }) => {
   return (
     <div className="w-screen">
       <iframe
-      className="w-screen aspect-video"
-        src={"https://www.youtube.com/embed/"+trailerVideoData?.key + "?autoplay=1&mute=1"}
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube.com/embed/" +
+          trailerVideoData?.key +
+          "?autoplay=1&mute=1&loop=1playlist=" +
+          trailerVideoData?.key
+        }
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
