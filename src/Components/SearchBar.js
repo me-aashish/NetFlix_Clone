e5c9c9ae1a92186974436dc565d0fea3823e650a
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-11-13 14:16:25 +0530

    star ted adding multilingual support on search page

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -6,7 +6,7 @@ const SearchBar = () => {
       <form className="bg-black w-1/2 grid grid-cols-12 bg-opacity-80 rounded-sm">
         <input
           type="text"
-          placeholder="What's Your Movie Mood Today?"
+          placeholder="What's Your Movie Mood Today ?"
           className="m-4 p-4 rounded-lg col-span-9 bg-gray-700 text-white"
         ></input>
         <button className="bg-[#e50914] m-4 p-4 rounded-lg text-white font-semibold col-span-3">
---
+++ src/utils/languageConstants.js
@@ -0,0 +1,22 @@
+const languages = {
+  en: {
+    search: "Search",
+    searchPlaceholder: "What's Your Movie Mood Today ?",
+    close: "Close",
+    logOut: "Log Out",
+  },
+  hin: {
+    search: "खोज",
+    searchPlaceholder: "आज आपकी मूवी का मूड क्या है ?",
+    close: "बंद करें",
+    logOut: "लॉग आउट",
+  },
+  spanish: {
+    search: "Buscar",
+    searchPlaceholder: "¿Cuál es tu estado de ánimo cinematográfico hoy ?",
+    close: "Cerca",
+    logOut: "Cerrar sesión",
+  },
+};
+
+export default languages;
