cd4b2338333f430b3cac9b735735ff0abac78c07
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-11-19 20:55:18 +0530

    stopped making app responsive

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -7,6 +7,7 @@ import Login from './Login'
 
 
 
+
 const Body = () => {
 
   const appRouter = createBrowserRouter([
--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -26,7 +26,7 @@ const Browse = () => {
       <Header />
       {isSearchContainerVisible ? (
         <Search />
-      ) : (
+      ) : ( 
         <>
           <MainBrowserContainer />
           <SecondaryBrowserContainer />
